1cf41237c681265df26daa48a4470646988a83cf
kimeasyn <kimeasyn@gmail.com>
2026-03-12 16:34:50 +0900

    feat: add EC2 start/stop to DAG

--- dags/retrain_pipeline.py
+++ dags/retrain_pipeline.py
@@ -1,11 +1,21 @@
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.amazon.aws.operators.ec2 import EC2StartInstanceOperator, EC2StopInstanceOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+
+INSTANCE_ID = os.environ.get("EC2_INSTANCE_ID")
+REGION = os.environ.get("EC2_REGION", "ap-northeast-2")
 
 EC2_BASE = "cd /home/ubuntu/app/settleup-category-classifier && source /home/ubuntu/app/.env && source venv/bin/activate"
 EC2_EXTRACT = f"{EC2_BASE} && python retrain/extract_data.py"
 EC2_TRAIN = f"{EC2_BASE} && python retrain/train_model.py"
 EC2_CONVERT = f"{EC2_BASE} && python retrain/convert_onnx.py"
+
+
+def wait_for_ec2():
+    """EC2 부팅 + Tailscale 연결 대기"""
+    time.sleep(60)
 
 with DAG(
     dag_id="retrain_pipeline",
@@ -14,6 +24,18 @@ with DAG(
     catchup=False,
     dagrun_timeout=timedelta(hours=6),
 ) as dag:
+
+    start_ec2 = EC2StartInstanceOperator(
+        task_id="start_ec2",
+        instance_id=INSTANCE_ID,
+        region_name=REGION,
+        aws_conn_id="aws_default",
+    )
+
+    wait = PythonOperator(
+        task_id="wait_for_boot",
+        python_callable=wait_for_ec2,
+    )
 
     extract = SSHOperator(
         task_id="extract_data",
@@ -38,5 +60,15 @@ with DAG(
         execution_timeout=timedelta(hours=1),
         cmd_timeout=None,
     )
+
+    stop_ec2 = EC2StopInstanceOperator(
+        task_id="stop_ec2",
+        instance_id=INSTANCE_ID,
+        region_name=REGION,
+        aws_conn_id="aws_default",
+        trigger_rule="all_done",
+    )
+
+    start_ec2 >> wait >> extract >> train >> convert >> stop_ec2
 
     extract >> train >> convert
